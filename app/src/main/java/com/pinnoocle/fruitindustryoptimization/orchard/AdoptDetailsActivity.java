1668e48fa1a1d67bdfc07d2f71254df40dbc431b
ganwanghua <1345895582@qq.com>
2021-01-21 17:01:14 +0800

    saaa

--- app/src/main/java/com/pinnoocle/fruitindustryoptimization/orchard/AdoptDetailsActivity.java
+++ app/src/main/java/com/pinnoocle/fruitindustryoptimization/orchard/AdoptDetailsActivity.java
@@ -1,72 +1,69 @@
 package com.pinnoocle.fruitindustryoptimization.orchard;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.FrameLayout;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
+import androidx.fragment.app.Fragment;
+
 import com.pinnoocle.fruitindustryoptimization.R;
 import com.pinnoocle.fruitindustryoptimization.common.BaseActivity;
-import com.youth.banner.Banner;
-import com.youth.banner.adapter.BannerImageAdapter;
-import com.youth.banner.holder.BannerImageHolder;
-import com.youth.banner.listener.OnPageChangeListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class AdoptDetailsActivity extends BaseActivity implements OnPageChangeListener {
+public class AdoptDetailsActivity extends BaseActivity {
 
-    private List<String> bannerList;
-    @BindView(R.id.goods_banner)
-    Banner banner;
-    @BindView(R.id.banner_indicator)
-    TextView bannerIndicator;
 
+    @BindView(R.id.fl_banner)
+    FrameLayout flBanner;
+    private List<Fragment> mList = new ArrayList<>();
+    private BannerImageFragment bannerImageFragment;
+    private BannerVideoFragment bannerVideoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         initTransparent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopt_details);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        bannerImageFragment = new BannerImageFragment();
+        bannerVideoFragment = new BannerVideoFragment();
+        mList.add(bannerImageFragment);
+        mList.add(bannerVideoFragment);
+        switchFragment(mList.get(0));
     }
 
 
-    private void initBanner(List<String> album) {
-        bannerList = new ArrayList<>();
-        bannerList = album;
-        banner.isAutoLoop(false)
-                .setAdapter(new BannerImageAdapter<String>(bannerList) {
-                    @Override
-                    public void onBindView(BannerImageHolder holder, String data, int position, int size) {
-                        //图片加载自己实现
-                        Glide.with(holder.itemView)
-                                .load(data)
-                                .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
-                                .into(holder.imageView);
-                    }
-                })
-                .isAutoLoop(false)
-                .addOnPageChangeListener(this);
-        bannerIndicator.setText("1/" + bannerList.size());
+    @OnClick({R.id.tv_1, R.id.tv_2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_1:
+                switchFragment(mList.get(0));
+                break;
+            case R.id.tv_2:
+                switchFragment(mList.get(1));
+                break;
+        }
     }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        int realposition = position + 1;
-        bannerIndicator.setText(realposition + "/" + bannerList.size());
+    private void switchFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_banner,fragment).commitAllowingStateLoss();
     }
 
     @Override
-    public void onPageScrollStateChanged(int state) {
+    public void onBackPressed() {
+        if(bannerVideoFragment!=null){
+            bannerVideoFragment.onBackPressed();
+        }
+        super.onBackPressed();
 
     }
 }
